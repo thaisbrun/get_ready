cd381827c821d6cfb239bd7f0a43fade07266251
Thaïs Brun <travailthais@outlook.fr>
2023-06-24 19:10:30 +0200

    Auto-formation du 24/06

--- lib/pages/lipsPage.dart
+++ lib/pages/lipsPage.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 
-class LipsPage extends StatelessWidget {
+class LipsPage extends StatefulWidget {
   const LipsPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:Text("Produits lèvres"),
-      ),
-      body: Center(
-        child:Text("Prochainement disponible"),
-      ),
-    );
+  State<LipsPage> createState() => _LipsPageState();
+}
+class _LipsPageState extends State<LipsPage>{
+  final lipsproducts = [
+    {
+      "product" : "Rouge à lèvres"
+    },
+    {
+      "product" : "Gloss"
+    },
+    {
+      "product" : "Crayon à lèvres"
+    }
+  ];
+  @override
+  Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title:Text("Produits lèvres"),
+        ),
+        body: Center(
+          child:ListView.builder(
+            itemCount: lipsproducts.length,
+            itemBuilder: (context,index){
+              final lipsProduct = lipsproducts[index];
+              final product = lipsProduct['product'];
+              return  Card(
+                child: ListTile(
+                  title: Text('$product'),
+                  trailing: Icon(Icons.open_in_new),
+                ),
+              );
+            }
+          ),
+        ),
+      );
   }
 }
