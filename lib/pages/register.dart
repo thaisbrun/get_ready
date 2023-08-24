199321fee62654cf6c1107297286fb151d629134
Thaïs Brun <travailthais@outlook.fr>
2023-08-24 17:48:50 +0200

    Authentification Firebase

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_ready/pages/connexion.dart';
+
+import 'homePage.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -14,21 +17,13 @@ class _RegisterState extends State<RegisterPage> {
 
   final _formKey = GlobalKey<FormState>();
 
-  final userNameController = TextEditingController();
-  final prenomController = TextEditingController();
   final mailController = TextEditingController();
-  final adressePostaleController = TextEditingController();
-  final telephoneController = TextEditingController();
   final mdpController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
-    userNameController.dispose();
-    prenomController.dispose();
     mailController.dispose();
-    adressePostaleController.dispose();
-    telephoneController.dispose();
     mdpController.dispose();
   }
   @override
@@ -40,40 +35,6 @@ class _RegisterState extends State<RegisterPage> {
         key: _formKey,
         child:Column(
           children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              child: TextFormField(
-              decoration: const InputDecoration(
-             labelText: 'Nom : ',
-              hintText: 'Entrez votre nom',
-              border:OutlineInputBorder()
-              ),
-              validator: (value){
-                if(value == null || value.isEmpty){
-                  return "Le nom ne peut pas être invalide. ";
-                }
-                return null;
-              },
-                controller: userNameController,
-            ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    labelText: 'Prénom : ',
-                    hintText: 'Entrez votre prénom',
-                    border:OutlineInputBorder(),
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return "Le prénom ne peut pas être invalide. ";
-                  }
-                  return null;
-                },
-                controller: prenomController,
-              ),
-            ),
             Container(
               margin: const EdgeInsets.only(bottom: 10),
               child: TextFormField(
@@ -95,40 +56,6 @@ class _RegisterState extends State<RegisterPage> {
               margin: const EdgeInsets.only(bottom: 10),
               child: TextFormField(
                 decoration: const InputDecoration(
-                  labelText: "Téléphone : ",
-                  hintText: "Entrez votre numéro",
-                  border:OutlineInputBorder(),
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return "Le numéro ne peut pas être invalide. ";
-                  }
-                  return null;
-                },
-                controller: telephoneController,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Adresse postale : ",
-                  hintText: "Entrer en euros",
-                  border:OutlineInputBorder(),
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return "Le prix ne peut pas être invalide. ";
-                  }
-                  return null;
-                },
-                controller: adressePostaleController,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              child: TextFormField(
-                decoration: const InputDecoration(
                   labelText: 'Mot de passe : ',
                   hintText: 'Entrez votre mdp',
                   border:OutlineInputBorder(),
@@ -143,41 +70,35 @@ class _RegisterState extends State<RegisterPage> {
               ),
             ),
             SizedBox(
-              width:double.infinity,
-            child:ElevatedButton(onPressed: () {
-              if(_formKey.currentState!.validate()){
-                final userName = userNameController.text;
-                final prenom = prenomController.text;
-                final mail = mailController.text;
-                final telephone = telephoneController.text;
-                final adressePostale = adressePostaleController.text;
-                final motDePasse = mdpController.text;
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text("Envoi en cours..."))
-                );
-
-                FocusScope.of(context).requestFocus(FocusNode());
-
-                //ajout dans la collection firebase
-               CollectionReference usersRef = FirebaseFirestore.instance.collection("Utilisateurs");
-                usersRef.add({
-                 'nom': userName,
-                 'prenom': prenom,
-                 'mail': mail,
-                 'telephone' : telephone,
-                 'adressePostale': adressePostale,
-                 'dateCreation': DateTime.now(),
-                 'activation': 1,
-                  'motDePasse' : motDePasse,
-                });
-              }
+              child: ElevatedButton(
+              onPressed: () async {
+                try {
+                  final credential = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                    email: mailController.text,
+                    password: mdpController.text,
+                  );
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  }
+                } catch (e) {
+                  print(e);
+                }
+                  FocusScope.of(context).requestFocus(FocusNode());
             },
               style:const ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(Colors.pink)
               ),
                 child: const Text("M'inscrire"),
             ),
-            ),
+  ),
             Center(
               child: CupertinoButton(
                 child: const Text("J'ai déjà un compte"),
@@ -189,7 +110,6 @@ class _RegisterState extends State<RegisterPage> {
                 },
     ),
             ),
-
           ],
         ),
       ),
