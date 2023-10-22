aab99a9ac4b7defef6c196f22f9f2ccbb525ca0c
Thaïs Brun <travailthais@outlook.fr>
2023-10-22 10:56:59 +0200

    avancement du 22/10

--- lib/pages/skinPage.dart
+++ lib/pages/skinPage.dart
@@ -3,7 +3,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_ready/main.dart';
 
+import 'EyesPage.dart';
+import 'browPage.dart';
 import 'getProduct.dart';
+import 'lipsPage.dart';
+import 'nailsPage.dart';
 
 class SkinPage extends StatefulWidget {
   const SkinPage({super.key, required this.title});
@@ -15,11 +19,19 @@ class SkinPage extends StatefulWidget {
 }
 
 class _SkinPageState extends State<SkinPage> {
+  int _selectedIndex = 0;
 
-
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.red[200]!,),
       body: Column(
         children: [
           Container(
@@ -79,6 +91,99 @@ class _SkinPageState extends State<SkinPage> {
             ),
           ),
         ],
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFFEF9A9A),
+              ),
+              child: Text('Get Ready'),
+            ),
+            ListTile(
+              title: const Text('Produits teint'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SkinPage(title: MyApp.appTitle)),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Produits yeux'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const EyesPage(
+                      title: MyApp.appTitle)),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Produits lèvres'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LipsPage(title: MyApp.appTitle)),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Produits sourcils'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const BrowPage(
+                      title: MyApp.appTitle)),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Produits ongles'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NailsPage(
+                      title: MyApp.appTitle)),
+                );
+              },
+            ),
+          ],
+        ),
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: 'Mon Panier',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle_sharp),
+            label: 'Mon Compte',
+          )
+        ],
+        selectedItemColor: Colors.red[200],
+
+
       ),
     );
 
